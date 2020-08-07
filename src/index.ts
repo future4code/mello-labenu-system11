a524de829c11f9cb03d0ce008fbea71612000952
Lourenço Passos <lo.passos93@gmail.com>
2020-08-07 19:33:53 -0300

    Progresso Missões

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
 import Student from "./Student";
-import {Teacher, SPECIALTY} from "./Teacher";
+import { Teacher, SPECIALTY } from "./Teacher";
+import { NightMission } from "./NightMission";
+import { FullTimeMission } from "./FullTimeMission";
 
-new Student(['nadar', 'brincar'], '25/02/1987', 'student@gmail.com', 'Mayara')
-new Student(['voar', 'ler'], '25/02/2001', 'student1@gmail.com', 'Lucas')
-new Teacher([SPECIALTY.REACT, SPECIALTY.POO, SPECIALTY.REDUX], '30/10/1993', 'teacher@hotmail.com', 'Oswaldo')
+new Student(["nadar", "brincar"], "25/02/1987", "student@gmail.com", "Mayara");
+new Student(["voar", "ler"], "25/02/2001", "student1@gmail.com", "Lucas");
+new Teacher(
+  [SPECIALTY.REACT, SPECIALTY.POO, SPECIALTY.REDUX],
+  "30/10/1993",
+  "teacher@hotmail.com",
+  "Oswaldo"
+);
+const mission1 = new NightMission(
+  "Mello",
+  1,
+  "16/04/2020",
+  "16/10/2020",
+  [],
+  [],
+  2
+);
+
+mission1.createNightMission(mission1);
+
+const mission2 = new FullTimeMission(
+  "Mello",
+  1,
+  "16/04/2020",
+  "16/10/2020",
+  [],
+  [],
+  2
+);
+
+mission2.createFullTimeMission(mission2);
